462d401bf1c7256419484184f9c3e90e36f2e341
Kushagra Rastogi <kushagrarastogi20004@gmail.com>
2025-01-27 02:10:35 +0530

    resume updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,9 +47,9 @@ const Hero = () => {
             <motion.a initial = {{y:-100 , opacity:0}} 
                 animate={{y:0, opacity:1}}
                 transition={{duration:1, delay:1.2}} 
-                href="/Kushagra_Rastogi-new_resume.pdf"
+                href="/Kushagra-Rastogi-resume.pdf"
                     target="_blank" rel="noopener noreferrer"
-                    download="/Kushagra_Rastogi-new_resume.pdf"
+                    download="/Kushagra-Rastogi-resume.pdf"
                      className="bg-white flex rounded-full p-4 text-sm text-slate-800 justify-center items-center sm:hidden mx-auto mt-20 mb-10 " >Download Resume</motion.a>
         </div>
     </div>
