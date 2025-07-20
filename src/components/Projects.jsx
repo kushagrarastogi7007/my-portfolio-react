bc0c2e590b0571217f0babac1009c460edf3e087
Kushagra Rastogi <kushagrarastogi20004@gmail.com>
2025-07-21 01:08:08 +0530

    tw updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -41,7 +41,7 @@ const Projects = () => {
                     <p className="mb-4 text-neutral-400">{project.description}
                     </p>
                     {project.technologies.map((tech, index) => (
-                        <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 font-medium text-purple-800">{tech}</span>
+                        <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 font-medium text-purple-800 flex flex-wrap">{tech}</span>
                     ))}
                     </motion.div>
                 </div>
